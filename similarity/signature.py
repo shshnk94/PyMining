b4448c05137d098f180b27e39d81e09c19fe79d6
Shashanka Subrahmanya <shashanka@Shashanks-MacBook-Pro.local>
2018-08-04 21:26:11 +0530

    UniHash in sign cal and added char_mat

--- similarity/signature.py
+++ similarity/signature.py
@@ -1,10 +1,37 @@
 import numpy as np
+import math
+import pdb
 
-def hash_1(x):
-	return (x + 1) % 5
+def get_prime(number):	
+	
+	flag = True
+	num = number + 1
 
-def hash_2(x):
-	return (3 * x + 1) % 5
+	while flag and num < 100:
+
+		i = 2
+		while (i <= math.sqrt(num)) and (num % i != 0):
+			i += 1
+
+		if i > math.sqrt(num):
+			flag = False
+		else:
+			num += 1
+
+	return num
+
+'''
+For any 'a' in the range of 1 to p-1 inclusive and 'b' in the range of 0 to p-1 inclusive we define 
+a universal family of hash functions where each function is parameterized with different values of 'a' and 'b'.
+
+Each hash function of this family is given by,
+	h(k, a, b) = ((ax + b) mod p) mod m)
+where 'p' is a prime next to the maximum value in the universe of valid keys.
+
+'''
+
+def hash(a, b, p, m, key):
+	return ((a * key + b) % p) % m
 
 if __name__ == "__main__":
 
@@ -12,16 +39,17 @@ if __name__ == "__main__":
 		matrix = np.array([line.split() for line in handle], dtype = int)
 
 	num_hash_fun = 2
+	p = get_prime(matrix.shape[0])
+	hash_parameters = [[np.random.randint(1, p-1), np.random.randint(0, p-1)] for i in range(num_hash_fun)]
 	signature_matrix = np.array([[np.inf] * matrix.shape[1] for i in range(num_hash_fun)])
 
 	for i, row in enumerate(matrix):
+		hash_values = [hash(a, b, p, matrix.shape[0], i) for a, b in hash_parameters]
 
-		hash_values = [hash_1(i), hash_2(i)]
+		#FIXME : Collision in hash value since small p and m`	
 		for j, col in enumerate(row):
-			if (col == 1) and (signature_matrix[0, j] > hash_values[0]):
-				signature_matrix[0, j] = hash_values[0]
-			if (col == 1) and (signature_matrix[1, j] > hash_values[1]):
-				signature_matrix[1, j] = hash_values[1]
+			for k in range(num_hash_fun):
+				if (col == 1) and (signature_matrix[k, j] > hash_values[k]):
+					signature_matrix[k, j] = hash_values[k]
 
-	
 	print(signature_matrix)	
