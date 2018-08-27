5ad71cfb117e243e9bfdc19f8c1b484703471b3d
Shashanka Subrahmanya <shshnk94@gmail.com>
2018-08-27 10:11:56 +0530

    Minor changes, simpler hash function

--- stream_algorithms/bloom_filters.py
+++ stream_algorithms/bloom_filters.py
@@ -1,10 +1,8 @@
 import numpy as np
 import sys
-sys.path.append("../")
-from similarity.prime import get_prime
 
-def hash(a, b, p, m, key):
-	return ((((a * key + b) % p) % m))
+def hash(a, b, m, key):
+	return (((a * key + b) % m))
 
 if __name__ == "__main__":
 	
@@ -21,38 +19,32 @@ if __name__ == "__main__":
 	print("Stream size")
 	stream_size = int(sys.stdin.readline())
 	
-	p = get_prime(filter_size)
-	hash_parameters = [[np.random.randint(1, p-1), np.random.randint(0, p-1)] for i in range(num_hash_functions)]
+	hash_parameters = [[np.random.randint(0, filter_size), np.random.randint(0, filter_size)] for i in range(num_hash_functions)]
 
 	#Initialising the bloom filter by setting the bits for the required key (in the inclusive range of 0 to 100) values
+	stored_values = []
 	for i in range(num_values):
 		
 		key = np.random.randint(0, 101)
-		print(key, end = " ")
+		stored_values.append(key)
 
 		#Hash the key across 'k' hash functions and set the bit to 1. Equivalent to cascaded bloom filters.
 		for [a, b] in hash_parameters:
-			print(hash(a, b, p, filter_size, key))
-			bloom_filter[hash(a, b, p, filter_size, key)] = 1
-	print()
-	print(bloom_filter)
+			bloom_filter[hash(a, b, filter_size, key)] = 1
 
 	#Check if the key exists in the filters when a stream of those keys (100, in this case) arrive.
 	allowed = []
 	disallowed = []
 
-	print("Enter the required stream size")
 	for i in range(stream_size):
 
 		key = np.random.randint(0, 101)
-		print("key", key)
 		
 		i = 0
 		while i < len(hash_parameters):
 			[a, b] = hash_parameters[i]
 			
-			print(hash(a, b, p, filter_size, key))
-			if bloom_filter[hash(a, b, p, filter_size, key)] == 0:
+			if bloom_filter[hash(a, b, filter_size, key)] == 0:
 				disallowed.append(key)
 				break
 			
@@ -60,7 +52,10 @@ if __name__ == "__main__":
 
 		if i == len(hash_parameters):
 			allowed.append(key)
-				
+			
+	print("Stored keys")
+	print(stored_values)
+	
 	print("Allowed keys")
 	print(allowed)
 	
